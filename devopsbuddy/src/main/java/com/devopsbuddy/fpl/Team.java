bce3d4a960546515dac07c56b1b2ad18d3986d46
gbanne12 <gbannerman14986@gmail.com>
2018-11-14 22:56:38 +0000

    Use an int instead of long for storing the team id

--- devopsbuddy/src/main/java/com/devopsbuddy/fpl/Team.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/fpl/Team.java
@@ -2,13 +2,13 @@ package com.devopsbuddy.fpl;
 
 public class Team {
 
-    private long id;
+    private int id;
 
-    public long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
