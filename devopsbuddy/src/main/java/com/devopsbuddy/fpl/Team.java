8dca46b3c66087b7a88559bf66fc056e478dae94
cjm864 <gary.bannerman@motorolasolutions.com>
2018-11-14 13:24:40 +0000

    Add a result page which is loaded on post request of the index page form

---
+++ devopsbuddy/src/main/java/com/devopsbuddy/fpl/Team.java
@@ -0,0 +1,15 @@
+package com.devopsbuddy.fpl;
+
+public class Team {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
--- devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,10 +1,14 @@
 package com.devopsbuddy.web.controllers;
 
 import com.devopsbuddy.fpl.Player;
+import com.devopsbuddy.fpl.Team;
 import com.devopsbuddy.fpl.Totalizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -12,14 +16,20 @@ import java.util.List;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String displayPlayerTotals(Model model) throws Exception {
         Totalizer totalizer = new Totalizer();
         List<Player> players = totalizer.getLeagueTable("235052");
         players.sort((p1, p2) -> p2.getTotal() - p1.getTotal());
 
         model.addAttribute("players", players);
+        model.addAttribute("team", new Team());
         return "index";
+    }
+
+    @PostMapping("/index")
+    public String greetingSubmit(@ModelAttribute Team team) {
+        return "result";
     }
 
 }
