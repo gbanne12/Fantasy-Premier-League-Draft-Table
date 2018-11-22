e72e5c99a013c763e095c8f0eb5a2e9687c24a03
gbanne12 <gbannerman14986@gmail.com>
2018-11-22 21:22:38 +0000

    Consolidate both post requests into loading a singular page now called result.html..

--- devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/WebController.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/WebController.java
@@ -5,7 +5,6 @@ import com.devopsbuddy.fpl.Player;
 import com.devopsbuddy.fpl.Team;
 import com.devopsbuddy.fpl.Totalizer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,8 +16,8 @@ import java.util.List;
 @org.springframework.stereotype.Controller
 public class WebController {
 
-    @Value("88888")
-    private String teamId;
+    @Value("77777")
+    private int teamId;
 
     @Value("11")
     private int monthId;
@@ -29,40 +28,33 @@ public class WebController {
         return "index";
     }
 
-    @PostMapping("/index")
+    @PostMapping("/result")
     public String submit(@ModelAttribute Team team, Model model) throws IOException {
-        teamId = Integer.toString(team.getId());
+        teamId = team.getId();
+        monthId = team.getMonth();
         Totalizer totalizer = new Totalizer();
-        List<Player> players = totalizer.getLeagueTable(teamId, getMonth(monthId));
+        List<Player> players = totalizer.getLeagueTable(Integer.toString(teamId), getMonth(monthId));
         players.sort((p1, p2) -> p2.getTotal() - p1.getTotal());
         model.addAttribute("players", players);
 
         return "result";
     }
 
-    @PostMapping("/month")
-    public String monthSelection(@ModelAttribute Team team, Model model) throws IOException {
-        monthId = team.getMonth();
-        Totalizer totalizer = new Totalizer();
-        List<Player> players = totalizer.getLeagueTable(teamId, getMonth(monthId));
-        players.sort((p1, p2) -> p2.getTotal() - p1.getTotal());
-        model.addAttribute("players", players);
-
-        return "month";
-    }
-
-
     private Month getMonth(int value) {
         Month month = Month.NOVEMBER;
-        switch(value) {
-            case 8 : month = Month.AUGUST;
-            break;
-            case 9: month = Month.SEPTEMBER;
-            break;
-            case 10: month = Month.OCTOBER;
-            break;
-            case 11: month = Month.NOVEMBER;
-            break;
+        switch (value) {
+            case 8:
+                month = Month.AUGUST;
+                break;
+            case 9:
+                month = Month.SEPTEMBER;
+                break;
+            case 10:
+                month = Month.OCTOBER;
+                break;
+            case 11:
+                month = Month.NOVEMBER;
+                break;
         }
         return month;
     }
