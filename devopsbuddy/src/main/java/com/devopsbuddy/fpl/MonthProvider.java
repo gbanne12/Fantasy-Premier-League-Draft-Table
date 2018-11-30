6ff1a7a643ebd8c6ea5b006daf50e02b17e0b3b4
gbanne12 <gbannerman14986@gmail.com>
2018-11-30 18:27:19 +0000

    Update the months of the season to include 2019.

--- devopsbuddy/src/main/java/com/devopsbuddy/fpl/MonthProvider.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/fpl/MonthProvider.java
@@ -6,14 +6,20 @@ import java.util.List;
 
 public class MonthProvider {
 
-    List<Month> months = new ArrayList<>();
+    private List<Month> months;
 
     public MonthProvider() {
+        months = new ArrayList<>();
         months.add(new Month("August", Calendar.AUGUST));
-        months.add(new Month("Sepetember", Calendar.SEPTEMBER));
+        months.add(new Month("September", Calendar.SEPTEMBER));
         months.add(new Month("October", Calendar.OCTOBER));
         months.add(new Month("November", Calendar.NOVEMBER));
         months.add(new Month("December", Calendar.DECEMBER));
+        months.add(new Month("January", Calendar.JANUARY));
+        months.add(new Month("February", Calendar.FEBRUARY));
+        months.add(new Month("March", Calendar.MARCH));
+        months.add(new Month("April", Calendar.APRIL));
+        months.add(new Month("May", Calendar.MAY));
     }
 
     public List<Month> getList() {
