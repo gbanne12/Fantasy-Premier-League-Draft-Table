b111ffed8bfc1021235a0f8a121573ca73cf0972
gbanne12 <gbannerman14986@gmail.com>
2018-11-30 18:29:45 +0000

    Add a default error page

--- devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/WebController.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/WebController.java
@@ -1,5 +1,6 @@
 package com.devopsbuddy.web.controllers;
 
+import com.devopsbuddy.exceptions.FplResponseException;
 import com.devopsbuddy.fpl.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,20 +9,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.spi.CalendarDataProvider;
 
 @Controller
 public class WebController {
 
-    MonthProvider monthProvider = new MonthProvider();
+    private MonthProvider monthProvider = new MonthProvider();
 
     @GetMapping("/")
-    public String initialise(Model model) {
-        model.addAttribute("userInput", new UserInput());
-
+    public String initialise(@ModelAttribute UserInput userInput, Model model) {
         List<Month> months = monthProvider.getList();
         model.addAttribute("months", months);
 
@@ -38,17 +35,38 @@ public class WebController {
 
         String teamId = Integer.toString(userInput.getId());
         GameweekMonth gameweekMonth = getMonthFromValue(userInput.getMonth());
-        League league = new League();
-        List<Player> players = league.getData(teamId, gameweekMonth);
-        players.sort((p1, p2) -> p2.getTotal() - p1.getTotal());
-        model.addAttribute("players", players);
 
-        return "result";
+        try {
+            League league = new League();
+            List<Player> players = league.getData(teamId, gameweekMonth);
+            players.sort((p1, p2) -> p2.getTotal() - p1.getTotal());
+            model.addAttribute("players", players);
+
+        } catch (FplResponseException e) {
+            return "team-not-found";
+        }
+
+       return "result";
     }
 
     private GameweekMonth getMonthFromValue(int value) {
         GameweekMonth gameweekMonth = GameweekMonth.NOVEMBER;
         switch (value) {
+            case 0:
+                gameweekMonth = GameweekMonth.JANUARY;
+                break;
+            case 1:
+                gameweekMonth = GameweekMonth.FEBRUARY;
+                break;
+            case 2:
+                gameweekMonth = GameweekMonth.MARCH;
+                break;
+            case 3:
+                gameweekMonth = GameweekMonth.APRIL;
+                break;
+            case 4:
+                gameweekMonth = GameweekMonth.MAY;
+                break;
             case 7:
                 gameweekMonth = GameweekMonth.AUGUST;
                 break;
