9e00e4fe3bec4dbccbdef03f3c6f785c801c4889
gbann <gary.bannerman@motorolasolutions.com>
2019-03-12 13:01:03 +0000

    Move logic for obtaining league data into the controller

--- src/main/java/com/devopsbuddy/model/League.java
+++ src/main/java/com/devopsbuddy/model/League.java
@@ -1,72 +1,12 @@
 package com.devopsbuddy.model;
 
 
-import com.devopsbuddy.exceptions.FplResponseException;
-import com.devopsbuddy.fpl.json.LeagueData;
-import com.devopsbuddy.fpl.json.PlayerData;
-import com.devopsbuddy.model.GameMonth;
-import com.devopsbuddy.model.Player;
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class League {
 
-    public List<Player> getOrderedList(String playerIdentifier, int month) throws FplResponseException {
-        List<Player> players = new ArrayList<>();
-        try {
-            PlayerData initialPlayer = new PlayerData(playerIdentifier);
-            LeagueData leagueData = new LeagueData(initialPlayer.getLeagueIdentifier());
-
-            List<String> playerIds = leagueData.getPlayerIds();
-            for (String id : playerIds) {
-                PlayerData player = new PlayerData(id);
-                players.add(new Player(player.getName(), player.getTeam(), player.getScore(getMonthFromValue(month))));
-            }
-
-        } catch (IOException exception) {
-            throw new FplResponseException("Unable to get data from the draft FPL API", exception);
-        }
+    public List<Player> getOrderedList(List<Player> players) {
         players.sort((p1, p2) -> p2.getTotal() - p1.getTotal());
         return players;
     }
-
-    private GameMonth getMonthFromValue(int value) {
-        GameMonth gameweekMonth = GameMonth.NOVEMBER;
-        switch (value) {
-            case 1:
-                gameweekMonth = GameMonth.JANUARY;
-                break;
-            case 2:
-                gameweekMonth = GameMonth.FEBRUARY;
-                break;
-            case 3:
-                gameweekMonth = GameMonth.MARCH;
-                break;
-            case 4:
-                gameweekMonth = GameMonth.APRIL;
-                break;
-            case 5:
-                gameweekMonth = GameMonth.MAY;
-                break;
-            case 8:
-                gameweekMonth = GameMonth.AUGUST;
-                break;
-            case 9:
-                gameweekMonth = GameMonth.SEPTEMBER;
-                break;
-            case 10:
-                gameweekMonth = GameMonth.OCTOBER;
-                break;
-            case 11:
-                gameweekMonth = GameMonth.NOVEMBER;
-                break;
-            case 12:
-                gameweekMonth = GameMonth.DECEMBER;
-                break;
-        }
-        return gameweekMonth;
-    }
-
 }
--- src/main/java/com/devopsbuddy/web/controllers/ResultController.java
+++ src/main/java/com/devopsbuddy/web/controllers/ResultController.java
@@ -1,6 +1,9 @@
 package com.devopsbuddy.web.controllers;
 
 import com.devopsbuddy.exceptions.FplResponseException;
+import com.devopsbuddy.fpl.json.LeagueData;
+import com.devopsbuddy.fpl.json.PlayerData;
+import com.devopsbuddy.model.GameMonth;
 import com.devopsbuddy.model.League;
 import com.devopsbuddy.model.Month;
 import com.devopsbuddy.model.Player;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -51,12 +56,70 @@ public class ResultController {
 
         userInput.setId(Integer.parseInt(id));
         try {
+            List<Player> players = getLeagueTable(id, monthValue);
             League league = new League();
-            List<Player> players = league.getOrderedList(id, Integer.parseInt(month));
-            model.addAttribute("players", players);
+            List<Player> ordered = league.getOrderedList(players);
+            //List<Player> players = league.getOrderedList(id, Integer.parseInt(month));
+            model.addAttribute("players", ordered);
         } catch (FplResponseException e) {
             return "team-not-found";
         }
         return "result";
     }
+
+    private List<Player> getLeagueTable(String playerIdentifier, int month) throws FplResponseException {
+        List<Player> players = new ArrayList<>();
+        try {
+            PlayerData initialPlayer = new PlayerData(playerIdentifier);
+            LeagueData leagueData = new LeagueData(initialPlayer.getLeagueIdentifier());
+
+            List<String> playerIds = leagueData.getPlayerIds();
+            for (String id : playerIds) {
+                PlayerData player = new PlayerData(id);
+                players.add(new Player(player.getName(), player.getTeam(), player.getScore(getMonthFromValue(month))));
+            }
+
+        } catch (IOException exception) {
+            throw new FplResponseException("Unable to get data from the draft FPL API", exception);
+        }
+        return players;
+    }
+
+    private GameMonth getMonthFromValue(int value) {
+        GameMonth gameweekMonth = GameMonth.NOVEMBER;
+        switch (value) {
+            case 1:
+                gameweekMonth = GameMonth.JANUARY;
+                break;
+            case 2:
+                gameweekMonth = GameMonth.FEBRUARY;
+                break;
+            case 3:
+                gameweekMonth = GameMonth.MARCH;
+                break;
+            case 4:
+                gameweekMonth = GameMonth.APRIL;
+                break;
+            case 5:
+                gameweekMonth = GameMonth.MAY;
+                break;
+            case 8:
+                gameweekMonth = GameMonth.AUGUST;
+                break;
+            case 9:
+                gameweekMonth = GameMonth.SEPTEMBER;
+                break;
+            case 10:
+                gameweekMonth = GameMonth.OCTOBER;
+                break;
+            case 11:
+                gameweekMonth = GameMonth.NOVEMBER;
+                break;
+            case 12:
+                gameweekMonth = GameMonth.DECEMBER;
+                break;
+        }
+        return gameweekMonth;
+    }
+
 }
